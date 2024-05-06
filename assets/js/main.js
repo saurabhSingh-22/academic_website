3de17c0a23ece7ec2267946e6b79cd9712726407
Saurabh singh <saurabhsinghlko9@gmail.com>
2024-05-06 20:49:11 +0530

    menu and style chnages

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,14 +19,14 @@
   /**
    * Mobile nav toggle
    */
-  const mobileNavToggleBtn = document.querySelector('.mobile-nav-toggle');
+  // const mobileNavToggleBtn = document.querySelector('.mobile-nav-toggle');
 
-  function mobileNavToogle() {
-    document.querySelector('body').classList.toggle('mobile-nav-active');
-    mobileNavToggleBtn.classList.toggle('bi-list');
-    mobileNavToggleBtn.classList.toggle('bi-x');
-  }
-  mobileNavToggleBtn.addEventListener('click', mobileNavToogle);
+  // function mobileNavToogle() {
+  //   document.querySelector('body').classList.toggle('mobile-nav-active');
+  //   mobileNavToggleBtn.classList.toggle('bi-list');
+  //   mobileNavToggleBtn.classList.toggle('bi-x');
+  // }
+  // mobileNavToggleBtn.addEventListener('click', mobileNavToogle);
 
   /**
    * Hide mobile nav on same-page/hash links
--- assets/js/newscript.js
+++ assets/js/newscript.js
@@ -1,13 +1,38 @@
 
-var i = 0;
-var txt = document.getElementById('typingtext').innerHTML;
-var speed = 50;
+// var i = 0;
+// var txt = document.getElementById('typingtext').innerHTML;
+// var speed = 50;
 
-function typeWriter() {
-  if (i < txt.length) {
-    document.getElementById("showtext").innerHTML += txt.charAt(i);
-    i++;
-    setTimeout(typeWriter, speed);
-  }
+// function typeWriter() {
+//   if (i < txt.length) {
+//     document.getElementById("showtext").innerHTML += txt.charAt(i);
+//     i++;
+//     setTimeout(typeWriter, speed);
+//   }
+// }
+// typeWriter();
+
+// sidenav
+function openNav() {
+  document.getElementById("mySidenav").style.width = "380px";
+  document.getElementById("sidelayer").style.opacity = 1;
+  document.getElementById("body").style.overflow = "hidden";
 }
-typeWriter();
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("sidelayer").style.opacity = 0;
+  document.getElementById("body").style.overflow = "auto";
+}
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  var btns = document.querySelectorAll('.btn');
+  btns.forEach(function (btn) {
+    btn.addEventListener('click', function () {
+      this.classList.toggle('active');
+      this.classList.toggle('not-active');
+    });
+  });
+});
